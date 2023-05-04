70d33123b8d4e81486796cfcef4f29219afae7ad
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-05-04 10:55:32 +0200

    add opensearch support

--- README.md
+++ README.md
@@ -14,3 +14,13 @@
       -Dquarkus.container-image.additional-tags=latest \
       -Dquarkus.jib.base-jvm-image=openjdk:17-jdk
 ```
+
+## run image locally
+```shell
+      docker run --rm -ti --name masterdata \
+        --network=openepcis-net -p 8080:8080 \
+        -e "QUARKUS_PROFILE=docker" \
+        -e "QUARKUS_OPENSEARCH_HOSTS=openepcis-opensearch-01:9200" \
+        -e "S3_ENDPOINT_OVERRIDE=http://openepcis-minio:9000" \
+        docker-registry.company-group.com/openepcis/master-data-quarkus-rest:1.0.0-SNAPSHOT
+```
--- master-data-service/src/main/java/net/medifit/masterdata/opensearch/OpenSearchRepository.java
+++ master-data-service/src/main/java/net/medifit/masterdata/opensearch/OpenSearchRepository.java
@@ -2,10 +2,12 @@ package net.medifit.masterdata.opensearch;
 
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
+import java.util.concurrent.CompletableFuture;
 import javax.enterprise.context.ApplicationScoped;
 import lombok.RequiredArgsConstructor;
 import net.medifit.masterdata.Repository;
-import org.opensearch.client.opensearch.OpenSearchClient;
+import org.eclipse.microprofile.context.ManagedExecutor;
+import org.opensearch.client.opensearch.OpenSearchAsyncClient;
 import org.opensearch.client.opensearch._types.query_dsl.Query;
 import org.opensearch.client.opensearch.core.*;
 
@@ -13,13 +15,34 @@ import org.opensearch.client.opensearch.core.*;
 @RequiredArgsConstructor
 public class OpenSearchRepository implements Repository {
 
-  private final OpenSearchClient client;
+  private final OpenSearchAsyncClient asyncClient;
+
+  private final ManagedExecutor executor;
 
   @Override
   public <T> Multi<T> search(String index, Class<T> cls, Query query) {
     try {
-      SearchResponse<T> searchResponse = client.search(s -> s.index(index).query(query), cls);
-      return Multi.createFrom().items(searchResponse.hits().hits().stream()).map(h -> h.source());
+      CompletableFuture<SearchResponse<T>> future =
+          asyncClient.search(s -> s.index(index).query(query), cls);
+      return Multi.createFrom()
+          .emitter(
+              em -> {
+                Uni.createFrom()
+                    .completionStage(future)
+                    .runSubscriptionOn(executor)
+                    .subscribe()
+                    .with(
+                        r -> {
+                          r.hits().hits().stream().forEach(item -> em.emit(item.source()));
+                          em.complete();
+                        },
+                        t -> {
+                          em.fail(t);
+                        });
+              });
+      // SearchResponse<T> searchResponse = client.search(s -> s.index(index).query(query), cls);
+      // return Multi.createFrom().items(searchResponse.hits().hits().stream()).map(h ->
+      // h.source());
     } catch (Throwable e) {
       return Multi.createFrom().failure(e);
     }
@@ -28,8 +51,26 @@ public class OpenSearchRepository implements Repository {
   @Override
   public <T> Multi<T> search(Class<T> cls, SearchRequest searchRequest) {
     try {
-      SearchResponse<T> searchResponse = client.search(searchRequest, cls);
-      return Multi.createFrom().items(searchResponse.hits().hits().stream()).map(h -> h.source());
+      CompletableFuture<SearchResponse<T>> future = asyncClient.search(searchRequest, cls);
+      return Multi.createFrom()
+          .emitter(
+              em -> {
+                Uni.createFrom()
+                    .completionStage(future)
+                    .runSubscriptionOn(executor)
+                    .subscribe()
+                    .with(
+                        r -> {
+                          r.hits().hits().stream().forEach(item -> em.emit(item.source()));
+                          em.complete();
+                        },
+                        t -> {
+                          em.fail(t);
+                        });
+              });
+      // SearchResponse<T> searchResponse = client.search(searchRequest, cls);
+      // return Multi.createFrom().items(searchResponse.hits().hits().stream()).map(h ->
+      // h.source());
     } catch (Throwable e) {
       return Multi.createFrom().failure(e);
     }
@@ -40,7 +81,7 @@ public class OpenSearchRepository implements Repository {
     try {
       final IndexRequest<T> indexRequest =
           new IndexRequest.Builder<T>().index(index).id(id).document(value).build();
-      return Uni.createFrom().item(client.index(indexRequest));
+      return Uni.createFrom().completionStage(asyncClient.index(indexRequest));
     } catch (Throwable e) {
       return Uni.createFrom().failure(e);
     }
@@ -50,7 +91,8 @@ public class OpenSearchRepository implements Repository {
   public Uni<DeleteResponse> delete(String index, String id) {
     try {
       return Uni.createFrom()
-          .item(client.delete(new DeleteRequest.Builder().index(index).id(id).build()));
+          .completionStage(
+              asyncClient.delete(new DeleteRequest.Builder().index(index).id(id).build()));
     } catch (Throwable e) {
       return Uni.createFrom().failure(e);
     }

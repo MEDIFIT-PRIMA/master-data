c4018a717bf75b4f0b2bc6a3e25bb606b6ef26e9
Sven Boeckelmann <sboeckelmann@benelog.com>
2023-08-29 21:26:22 +0200

    fix data generation for medifit work in progress

--- master-data-rest-api/src/main/java/net/medifit/masterdata/api/RESTApplication.java
+++ master-data-rest-api/src/main/java/net/medifit/masterdata/api/RESTApplication.java
@@ -8,10 +8,33 @@ import jakarta.inject.Singleton;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.Application;
+import org.eclipse.microprofile.openapi.annotations.Components;
 import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeIn;
+import org.eclipse.microprofile.openapi.annotations.enums.SecuritySchemeType;
 import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.security.SecurityScheme;
 
-@OpenAPIDefinition(info = @Info(title = "MasterData API", version = "1.0.0"))
+@OpenAPIDefinition(info = @Info(title = "MasterData API", version = "1.0.0"),
+        components =
+@Components(
+        securitySchemes = {
+                @SecurityScheme(
+                        securitySchemeName = "bearerAuth",
+                        type = SecuritySchemeType.HTTP,
+                        scheme = "bearer",
+                        bearerFormat = "JWT"),
+                @SecurityScheme(
+                        securitySchemeName = "apiKey",
+                        type = SecuritySchemeType.APIKEY,
+                        in = SecuritySchemeIn.HEADER,
+                        apiKeyName = "API-KEY"),
+                @SecurityScheme(
+                        securitySchemeName = "apiKeySecret",
+                        type = SecuritySchemeType.APIKEY,
+                        in = SecuritySchemeIn.HEADER,
+                        apiKeyName = "API-KEY-SECRET")
+        }))
 @ApplicationPath("/")
 public class RESTApplication extends Application {
 
